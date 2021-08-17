83f5d7045dba0b045462d8a790ffb7a3c0bcf3c5
dunfan.lu <dunfan.lu@taichi.graphics>
2021-08-17 02:57:45 -0400

    ..

--- examples/ggui_examples/demo/fluids3d/box_data.py
+++ examples/ggui_examples/demo/fluids3d/box_data.py
@@ -1,11 +1,22 @@
-x_min = 0
-y_min = 0
-z_min = 0
+from taichi.lang.impl import indices
 
-x_max = 0
-y_max = 0
-z_max = 0
+
+x_min = -1
+y_min = -1
+z_min = -1
+
+x_max = 1
+y_max = 1
+z_max = 1
 
 verts = [
-    [x_min, x_min, x_min],
+    [x_min, y_min, z_min],
+    [x_min, y_min, z_max],
+    [x_min, y_max, z_min],
+    [x_min, y_max, z_max],
+
+    [x_max, y_min, z_min],
+    [x_max, y_min, z_max],
+    [x_max, y_max, z_min],
+    [x_max, y_max, z_max],
 ]
