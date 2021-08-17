579c2a1c88223e05d73953b90bcc8fca75018c8e
dunfan.lu <dunfan.lu@taichi.graphics>
2021-08-17 05:23:09 -0400

    track camera wip

--- python/taichi/ui/camera.py
+++ python/taichi/ui/camera.py
@@ -4,12 +4,20 @@
 class Camera:
     def __init__(self,ptr):
         self.ptr = ptr
+        self.curr_pos = (0,0,0)
+        self.curr_lookat = (0,0,1)
+        self.curr_up = (0,1,0)
+
     def position(self,x,y,z):
+        self.curr_position = (x,y,z)
         self.ptr.position(x,y,z)
     def lookat(self,x,y,z):
+        self.curr_lookat = (x,y,z)
         self.ptr.lookat(x,y,z)
     def up(self,x,y,z):
+        self.curr_up = (x,y,z)
         self.ptr.up(x,y,z)
+
     def projection_mode(self,mode):
         self.ptr.projection_mode(mode)
     def fov(self,fov):
@@ -26,3 +34,6 @@ class Camera:
         self.ptr.z_near(z_near)
     def z_near(self,z_far):
         self.ptr.z_far(z_far)
+
+    def track_user_inputs(self,window):
+        self.ptr.track_user_inputs
