a42252e0ab56534a6430aaf2b091fbeccca2e278
unknown <ldfrank533@outlook.com>
2021-10-08 18:54:44 +0100

    wip

---
+++ taichi/ui/common/field_info.cpp
@@ -0,0 +1,15 @@
+#include "taichi/ui/common/field_info.h"
+
+namespace taichi{
+
+namespace ui{
+
+using namespace taichi::lang;
+
+DevicePtr get_device_ptr(SNode* snode){
+    return kDeviceNullPtr;
+}
+
+}
+
+}
